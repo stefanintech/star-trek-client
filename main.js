edacc10e595662a529515a24e86ccb720b69c859
Stefan <stefantaitano@gmail.com>
2022-10-04 21:03:11 -0600

    revert specie name

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ async function apiRequest(){
         const data = await response.json()
         console.log(data)
 
-        document.getElementById('alienName').innerText = data.name
+        document.getElementById('alienName').innerText = data.speciesName
         document.getElementById('alienWorld').innerText = data.homeworld
         document.getElementById('alienFeatures').innerText = data.features
         document.getElementById('alienFacts').innerText = data.interestingFact
         document.getElementById('alienExamples').innerText = data.notableExample
 
         document.getElementById('alienImage').src = data.image
-        document.getElementById('alienCaption').innerText = data.name
+        document.getElementById('alienCaption').innerText = data.speciesName
     } catch(error) {
         console.log(error)
     }
